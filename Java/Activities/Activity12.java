d34bf5e660e7be1ca954b8712e03b2db0ea7c45b
KeerthiNattala <116861511+KeerthiNattala@users.noreply.github.com>
2022-10-31 22:17:22 +0530

    Add files via upload

---
+++ Java/Activities/Activity12.java
@@ -0,0 +1,18 @@
+package examples;
+
+interface Addable {
+    int add(int a, int b);
+}
+
+public class Activity12 {
+    public static void main(String[] args) {
+
+        // Lambda expression without return keyword.
+        Addable ad1 = Integer::sum;
+        System.out.println(ad1.add(10, 20));
+
+        // Lambda expression with return keyword.
+        Addable ad2 = Integer::sum;
+        System.out.println(ad2.add(100, 200));
+    }
+}
